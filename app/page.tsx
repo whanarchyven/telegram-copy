a16f27d817101e18e87462cb64f6e3ae34ce910b
Ivan Zaburdaev <ivanlom227@gmail.com>
2024-02-21 01:32:36 +0300

    added redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client"
 
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
 export default function Home() {
 
+    const router=useRouter()
+
+    useEffect(()=>{
+        router.push('/chats')
+    })
     return (
         <main className={'w-full h-full flex items-center justify-center'}>
             <div>
