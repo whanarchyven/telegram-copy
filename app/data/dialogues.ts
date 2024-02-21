fe3a889f16abb11187501f10be442da012a61737
Ivan Zaburdaev <ivanlom227@gmail.com>
2024-02-21 12:32:35 +0300

    added reviews to chats

--- app/data/dialogues.ts
+++ app/data/dialogues.ts
@@ -2,7 +2,7 @@ export const dialogues = [
     {
         id: 'new_client_registration',
         name: 'Newbies',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'Hi, I\'m new here. How do I register?',
@@ -48,7 +48,21 @@ export const dialogues = [
             },
             {
                 request: 'Thank you!',
-                answers: ['Have Fun! And i will be happy to assist you whenever you want.'],
+                answers: ['Have Fun! And i will be happy to assist you whenever you want.', '[✓] Customer: greeted\n' +
+                '[✓] Manager: Welcomed the customer, reflecting warmth and a positive attitude\n' +
+                '[✓] Manager: Expressed enthusiasm and friendliness in communication\n' +
+                '[✓] Manager: Politely initiated dialogue, determining the customer\'s readiness for a bonus offer\n' +
+                '[✓] Manager: Quick response to the customer\'s request\n' +
+                '[✓] Manager: Inquired about the customer\'s preferences regarding bonuses to offer the most suitable option\n' +
+                '[✓] Manager: Provided alternative bonus options after the requested bonus could not be activated\n' +
+                '[✓] Manager: Responded to the customer\'s desire quickly and offered specific options\n' +
+                '[✓] Customer: Asked clarifying questions about the bonus conditions or additional offers\n' +
+                '[✓] Manager: Clearly specified the steps needed to activate and use the bonus\n' +
+                '[✓] Manager: Explained the conditions for activating and using the bonus, considering the personal offer\n' +
+                '[✓] Customer: Expressed gratitude for the help and information about additional opportunities\n' +
+                '[✓] Manager: Offered additional interaction after solving the main problem\n' +
+                '[✓] Customer: said goodbye, expressing pleasure from the conversation\n' +
+                '[✓] Manager: said goodbye, reminding of availability for any future requests'],
                 tolerance: 70,
                 delay: 67
             },
@@ -57,7 +71,7 @@ export const dialogues = [
     {
         id: 'making_deposit_and_receiving_bonuses',
         name: 'Deposit',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'How do I make a deposit?',
@@ -73,7 +87,19 @@ export const dialogues = [
             },
             {
                 request: 'How do I claim the bonus?',
-                answers: ['The bonus will be automatically credited to your account after your deposit. You can check the "Bonuses" section in your account to see your available bonuses.'],
+                answers: ['The bonus will be automatically credited to your account after your deposit. You can check the "Bonuses" section in your account to see your available bonuses.','[✓] Client: Initiates conversation with a greeting and expresses interest in making a deposit.\n' +
+                '[✓] Agent: Warmly greets the client, showing enthusiasm and readiness to assist.\n' +
+                '[✓] Client: Inquires about the process of making a deposit.\n' +
+                '[✓] Agent: Clearly explains the deposit process, mentioning the "Banking" section and available payment methods.\n' +
+                '[✓] Client: Asks about minimum deposit requirements.\n' +
+                '[✓] Agent: Specifies the minimum deposit amount and informs about automatic bonus crediting after deposit.\n' +
+                '[✓] Client: Seeks clarification on how to claim bonuses.\n' +
+                '[✓] Agent: Guides client to check the "Bonuses" section in their account for available promotions and bonuses.\n' +
+                '[✓] Agent: Confirms that client understands the instructions given for making deposits and claiming bonuses.\n' +
+                '[✓] Client: Expresses satisfaction with the provided information and readiness to proceed with a deposit.\n' +
+                '[✓] Agent: Offers further assistance or information about promotions, aiming to enhance client’s experience beyond immediate query resolution.\n' +
+                '[ ] Client: Thanks agent for helpful guidance, indicating successful service interaction. \n' +
+                '[✓] Both parties exchange goodbyes, concluding the conversation on a positive note.'],
                 tolerance: 70,
                 delay: 118
             },
@@ -82,7 +108,7 @@ export const dialogues = [
     {
         id: 'technical_issues_with_games_or_bets',
         name: 'Bug Issues',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'I\'m experiencing a glitch in one of the games. What should I do?',
@@ -110,7 +136,18 @@ export const dialogues = [
             },
             {
                 request: 'No thx! Bye',
-                answers: ['Bye!'],
+                answers: ['Bye!','[✓] Client: greeted\n' +
+                '[✓] Manager: Welcomed the client, reflecting warmth and a positive attitude\n' +
+                '[✓] Manager: Confirmed successful interaction with the client\n' +
+                '[✓] Manager: Actively maintained dialogue by asking clarifying questions\n' +
+                '[✓] Manager: Responded quickly to the client\'s request\n' +
+                '[✓] Client: Described the problem encountered, providing necessary information for resolution\n' +
+                '[✓] Manager: Suggested initial steps to resolve the technical issue (clearing cache and cookies)\n' +
+                '[✓] Client: Followed the manager\'s recommendations and reported the results\n' +
+                '[✓] Manager: Offered alternative solutions and compensation for the inconvenience (e.g., free spins)\n' +
+                '[✓] Client: Expressed gratitude for the assistance\n' +
+                '[✓] Manager: Offered additional help or information on other products/games\n' +
+                '[✓] Client and Manager: said goodbye, concluding the dialogue on a positive note'],
                 tolerance: 70,
                 delay: 42
             },
@@ -119,7 +156,7 @@ export const dialogues = [
     {
         id: 'withdrawal_request_inquiries',
         name: 'Withdrawal Request',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'I requested a withdrawal, but it hasn\'t been credited to my bank account yet.',
@@ -141,7 +178,16 @@ export const dialogues = [
             },
             {
                 request: 'Alright, I\'ll wait for the update. Can I make another deposit while waiting for the withdrawal?',
-                answers: ['Ok! Write me, and i can give you additional bonuses! If you need any further assistance, don\'t hesitate to contact us. Have a good day!'],
+                answers: ['Ok! Write me, and i can give you additional bonuses! If you need any further assistance, don\'t hesitate to contact us. Have a good day!','[✓] Client: greeted\n' +
+                '[✓] Manager: Welcomed the client, reflecting warmth and a positive attitude\n' +
+                '[✓] Manager: Expressed enthusiasm and friendliness in communication\n' +
+                '[✓] Manager: Quick response to the client\'s request\n' +
+                '[✓] Client: Expressed a desire to obtain information about the status of their withdrawal request\n' +
+                '[✓] Manager: Was able to successfully contact the required colleague or check the information on the client\'s request\n' +
+                '[✓] Manager: Politely initiated dialogue, clarifying details of the client\'s withdrawal request\n' +
+                '[✓] Manager: Confirmed the status of the withdrawal request and explained the verification and waiting process\n' +
+                '[✓] Client: Expressed satisfaction with the manager\'s detailed explanation and gratitude for the assistance\n' +
+                '[✓] Manager: Offered additional help if the client has further questions or problems'],
                 tolerance: 70,
                 delay: 196
             },
@@ -150,7 +196,7 @@ export const dialogues = [
     {
         id: 'concerns_about_gambling_addiction',
         name: 'Addiction',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'I think I need to take a break from gambling. Can you help me set up a self-exclusion?',
@@ -178,7 +224,18 @@ export const dialogues = [
             },
             {
                 request: 'I will. Goodbye for now.',
-                answers: ['Goodbye, and take care. We\'re here if you need us.'],
+                answers: ['Goodbye, and take care. We\'re here if you need us.','[✓] Client: greeted\n' +
+                '[✓] Manager: Welcomed the client, reflecting warmth and a positive attitude\n' +
+                '[✓] Manager: Politely initiated dialogue, determining the client\'s readiness to discuss their issue or request\n' +
+                '[✓] Manager: Quick response to the client\'s request\n' +
+                '[✓] Manager: Provided clear and complete information about the self-exclusion process\n' +
+                '[✓] Client: Expressed their desire to set a self-exclusion period\n' +
+                '[✓] Manager: Confirmed successful application of self-exclusion and explained the next steps after the self-exclusion period ends\n' +
+                '[✓] Client: Expressed gratitude for the help and support\n' +
+                '[✓] Manager: Reflected empathy and support in communication, emphasizing the importance of the client\'s well-being\n' +
+                '[✓] Manager: Offered the possibility of contacting the customer support department in the future for clarification of further actions or in case of questions\n' +
+                '[✓] Client: said goodbye, expressing satisfaction with the received help\n' +
+                '[✓] Manager: said goodbye, reminding of the availability of support services for any further needs\n'],
                 tolerance: 70,
                 delay: 67
             },
@@ -187,7 +244,7 @@ export const dialogues = [
     {
         id: 'closing_an_account_upon_client_request',
         name: 'Closing Account',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'I need to close my account immediately.',
@@ -215,7 +272,17 @@ export const dialogues = [
             },
             {
                 request: 'I will. Goodbye.',
-                answers: ['Goodbye, and take care.'],
+                answers: ['Goodbye, and take care.','[✓] Client: Initiated the conversation, expressing a desire to close the account\n' +
+                '[✓] Manager: Showed empathy and understanding towards the client\'s decision\n' +
+                '[✓] Manager: Asked the client for the reason for closing the account to understand the context and the possibility of improving the service\n' +
+                '[✓] Manager: Requested confirmation of personal data for client identity verification and security of the process\n' +
+                '[✓] Client: Provided the required information for verification\n' +
+                '[✓] Manager: Confirmed receipt of information and started the account closure process\n' +
+                '[✓] Manager: Informed the client about the time frame for processing the account closure request\n' +
+                '[✓] Client: Expressed gratitude for the prompt resolution of the issue\n' +
+                '[✓] Manager: Offered help in the future and reminded of the availability of support services for any additional questions or needs\n' +
+                '[✓] Manager: Politely said goodbye, emphasizing readiness to help at any time\n' +
+                '[✓] Client: Confirmed satisfaction with the service without directly questioning the quality of service'],
                 tolerance: 70,
                 delay: 46
             },
@@ -224,7 +291,7 @@ export const dialogues = [
     {
         id: 'verifying_or_changing_customer_contact_details',
         name: 'Changing Contact',
-        startMessage:'Hi! I will help you with any question',
+        startMessage: 'Hi! I will help you with any question',
         body: [
             {
                 request: 'I need to update my email address. How can I do that?',
@@ -246,7 +313,17 @@ export const dialogues = [
             },
             {
                 request: 'No, that\'s all for now. Thank you!',
-                answers: ['You\'re welcome! If you have any other questions or need further assistance, feel free to contact us. Have a great day!', 'Goodbye!', 'Goodbye!'],
+                answers: ['You\'re welcome! If you have any other questions or need further assistance, feel free to contact us. Have a great day!', 'Goodbye!','[✓] Client: Greeted and immediately moved to the essence of the request.\n' +
+                '[✓] Manager: Effectively greeted the client and expressed readiness to help.\n' +
+                '[✓] Manager: Clearly requested current and new contact information without unnecessary questions.\n' +
+                '[✓] Client: Quickly and accurately provided the required information (current and new email addresses).\n' +
+                '[✓] Manager: Requested additional information/confirmation for client identity verification (answer to a security question or last digits of the phone number).\n' +
+                '[✓] Client: Precisely provided the requested information for verification.\n' +
+                '[✓] Manager: Confirmed successful email change and offered further assistance.\n' +
+                '[✓] Client: Expressed satisfaction with the resolution of the issue, had no more questions.\n' +
+                '[✓] Manager: Offered services for the future, wished a good day, and correctly concluded the conversation.\n' +
+                '[✓] Client: Thanked for the help and said goodbye.\n' +
+                '[✓] Manager: Politely said goodbye to the client.'],
                 tolerance: 70,
                 delay: 182
             },
